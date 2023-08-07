74fcc52b946a86801d55af1a92841603fbd97009
branco <branco.garcia2020@alu.uct.cl>
2023-08-07 00:03:40 -0400

    login y que pasa

---
+++ index.js
@@ -0,0 +1,9 @@
+const math = require("./apps")
+
+console.log(math);
+
+/*
+console.log(math.add(2,2));
+console.log(math.substract(2,2));
+console.log(math.multiply(2,2));
+console.log(math.divide(2,2)); */
